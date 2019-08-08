51c17a0db45658b9ba220997ab965b7b0efae2cd
sstambat12 <51150967+sstambat12@users.noreply.github.com>
2019-08-07 21:51:46 -0700

    Add project files.

---
+++ ChessAutomation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ChessAutomation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           // int num = 0;
+            for (int i = 0; i<8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    if (((i+j) % 2) == 1)
+                        Console.Write("O");
+                    else
+                        Console.Write("X");
+                }
+
+                Console.Write("\n");
+
+            }
+        }
+    }
+}
